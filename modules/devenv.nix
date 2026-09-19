ab0d8a52208bd3c00c14cbab76d46a4ff4e37cf5
kurt <50203404+elkurto343@users.noreply.github.com>
2026-09-19 13:24:46 +1000

    devenv: set DEVENV_BIN, install direnvrc directly

--- modules/devenv.nix
+++ modules/devenv.nix
@@ -1,8 +1,16 @@
 {
   lib,
+  pkgs,
   config,
   ...
-}: {
+}: let
+  cfg = config.programs.devenv;
+
+  direnvrc = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/cachix/devenv/v${cfg.package.version}/devenv/direnvrc";
+    hash = "sha256-p9dGsc/+ElCjLgTRoTFudKo+vcVNANKdvJMLReyteg8=";
+  };
+in {
   programs.devenv = {
     enable = lib.mkDefault true;
     enableBashIntegration = lib.mkDefault false;
@@ -11,7 +19,11 @@
     enableNushellIntegration = lib.mkDefault false;
   };
 
-  programs.direnv.stdlib = lib.mkIf (config.programs.devenv.enable && config.programs.direnv.enable) ''
-    eval "$(${lib.getExe config.programs.devenv.package} direnvrc)"
+  programs.direnv.stdlib = lib.mkIf (cfg.enable && config.programs.direnv.enable) ''
+    export DEVENV_BIN=${lib.getExe cfg.package}
   '';
+
+  xdg.configFile."direnv/lib/devenv.sh" = lib.mkIf (cfg.enable && config.programs.direnv.enable) {
+    source = direnvrc;
+  };
 }
